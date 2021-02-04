fc3009d5e876f1cad576eb5492a7736d0f84b536
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2021-02-04 12:59:19 +0200

    09. Use Providers & ChangeNotifer in Nested Model

--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -11,10 +11,13 @@ class ProductsGrid extends StatelessWidget {
     return GridView.builder(
       padding: const EdgeInsets.all(10),
       itemCount: products.length,
-      itemBuilder: (context, index) => ProductItem(
-        products[index].id,
-        products[index].title,
-        products[index].imageUrl,
+      itemBuilder: (context, index) => ChangeNotifierProvider(
+        create: (BuildContext context) => products[index],
+        child: ProductItem(
+            // products[index].id,
+            // products[index].title,
+            // products[index].imageUrl,
+            ),
       ),
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 2,
