0552dd758c3d77e0649da95e0908ed2100ad9047
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2021-02-04 19:45:35 +0200

    12. Manage displayed data in Provider class (App-wide Globally)

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -37,8 +37,22 @@ class Products with ChangeNotifier {
     ),
   ];
 
+  var _showFavoritesOnly = false;
+
   List<Product> get items {
+    if (_showFavoritesOnly)
+      return _items.where((productItem) => productItem.isFavorite).toList();
     return [..._items];
+  }
+
+  void showFavoritesOnly() {
+    _showFavoritesOnly = true;
+    notifyListeners();
+  }
+
+  void showAll() {
+    _showFavoritesOnly = false;
+    notifyListeners();
   }
 
   Product findById(String id) {
--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,12 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/providers/products.dart';
 import 'package:shop_app/widgets/products_grid.dart';
+
+enum FilterOption {
+  Favorits,
+  All,
+}
 
 class ProductsOverviewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final productsData = Provider.of<Products>(context, listen: false);
     return Scaffold(
       appBar: AppBar(
         title: Text('MyShop'),
+        actions: [
+          PopupMenuButton(
+            onSelected: (FilterOption selectdValue) {
+              // print(selectdValue);
+              if (selectdValue == FilterOption.Favorits)
+                productsData.showFavoritesOnly();
+              else
+                productsData.showAll();
+            },
+            icon: Icon(Icons.more_vert),
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                child: Text('Only Favorites'),
+                value: FilterOption.Favorits,
+              ),
+              PopupMenuItem(
+                child: Text('Show All'),
+                value: FilterOption.All,
+              ),
+            ],
+          ),
+        ],
       ),
       body: ProductsGrid(),
     );
