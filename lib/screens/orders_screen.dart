dd182dc0ff87ead666c091f16d079c31b2127caa
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2021-02-05 08:46:16 +0200

    24. Build App Drawer & add it to Orders & Shop Screens

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop_app/providers/orders.dart' show Orders;
+import 'package:shop_app/widgets/app_drawer.dart';
 import 'package:shop_app/widgets/order_item.dart';
 
 class OrdersScreen extends StatelessWidget {
@@ -12,6 +13,7 @@ class OrdersScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Your Orders'),
       ),
+      drawer: AppDrawer(),
       body: ListView.builder(
         itemCount: orderData.orders.length,
         itemBuilder: (context, index) => OrderItem(orderData.orders[index]),
