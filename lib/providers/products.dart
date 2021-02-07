25de4cb21892cafdf9edf89fcc22142d89150a79
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2021-02-07 15:04:48 +0200

    37. Save new Products

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -63,8 +63,16 @@ class Products with ChangeNotifier {
     return _items.firstWhere((product) => product.id == id);
   }
 
-  void addProduct() {
-    // _items.add(value);
+  void addProduct(Product product) {
+    final newProduct = Product(
+      id: DateTime.now().toString(),
+      title: product.title,
+      description: product.description,
+      price: product.price,
+      imageUrl: product.imageUrl,
+    );
+    _items.add(newProduct);
+    // _items.insert(0, newProduct); // at the start of the list
     notifyListeners();
   }
 }
