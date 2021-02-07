fb4069a7c81d5adfc3abe76cb7da4cc1508942d2
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2021-02-07 08:41:34 +0200

    30. Manage Form Input Focus

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -7,6 +7,7 @@ class EditProductScreen extends StatefulWidget {
 }
 
 class _EditProductScreenState extends State<EditProductScreen> {
+  final _priceFocusNode = FocusNode();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,6 +22,21 @@ class _EditProductScreenState extends State<EditProductScreen> {
               TextFormField(
                 decoration: InputDecoration(labelText: 'Title'),
                 textInputAction: TextInputAction.next,
+                // No need for next argument (onFieldSubmitted) to move to the
+                // next TextFormField in the Form. [1. Comment and Try]
+                // You just need to add the next Field and it will get focus.
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_priceFocusNode);
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Price'),
+                textInputAction: TextInputAction.next,
+                keyboardType: TextInputType.number,
+                // No need for next argument (focusNode) to get focus in here.
+                // Automatically focus in when the previous TextFormField hit
+                // Next button in soft keyboard. [1. Comment and Try]
+                focusNode: _priceFocusNode,
               ),
             ],
           ),
