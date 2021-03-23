a133ea3562a32bb97a7f52ec99a3b058425442a1
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2021-03-23 03:01:04 +0200

    60. Improve the code to deal with unrelated changes avoiding refetching while rebuilding

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -4,9 +4,37 @@ import 'package:shop_app/providers/orders.dart' show Orders;
 import 'package:shop_app/widgets/app_drawer.dart';
 import 'package:shop_app/widgets/order_item.dart';
 
-class OrdersScreen extends StatelessWidget {
+// 1. Convert to StatefulWidget.
+class OrdersScreen extends StatefulWidget {
   static const routeName = '/orders';
 
+  @override
+  _OrdersScreenState createState() => _OrdersScreenState();
+}
+
+class _OrdersScreenState extends State<OrdersScreen> {
+  // 2. Add a property (Variable) of type Future.
+  Future _ordersFuture;
+
+  // 3. Add new method which return a Future.
+  // 4. In this method return the result fo this call to fetch and set orders.
+  Future _obtainOrdersFuture() {
+    return Provider.of<Orders>(context, listen: false).fetchOrders();
+  }
+
+  // 5. You should add initState, set the result of method to the eailer variable.
+  @override
+  void initState() {
+    _ordersFuture = _obtainOrdersFuture();
+    super.initState();
+  }
+
+  // If you had somethinng (some other state you're managing or anything like that)
+  // in this widget which cause build method to run again (rebuild),then fetch and set
+  // orders would of course, also be executed.
+  // We might want to avoid if just something else changed in this widget,
+  // if there is no reason to fetch new orders. You don't want to fetch new orders
+  // just because something else changed in this widget, which doesn't affect the orders.
   @override
   Widget build(BuildContext context) {
     // This print statement to be sure the build method should now build one time.
@@ -24,7 +52,8 @@ class OrdersScreen extends StatelessWidget {
           // And it takes a builder which will get the current snapshot to current state
           // of your future so that you can build different content based on what your future return.
           FutureBuilder(
-        future: Provider.of<Orders>(context, listen: false).fetchOrders(),
+        // Set future using variable inisilized in initState()
+        future: _ordersFuture,
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting)
             return Center(
